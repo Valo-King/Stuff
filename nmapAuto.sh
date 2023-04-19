70734597c0ade0569d45484bc87848478d58ffb2
Valo King <valo.l.king@gmail.com>
2023-04-19 13:24:14 -0400

    Edited the functionality of nmapAuto
    Now nmapAuto takes user input. It makes the process less automagical and
    unschedulable, but it's more usable now. I might fork the two versions.

--- nmapAuto.sh
+++ nmapAuto.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-scanrange="192.168.0.0/24"
-filename=$(date +%F)
+echo -n "Enter the IP range or host you need to scan."
+read scanrange
+echo -n "Enter the filename you'd like to edit."
+read filename
+
 echo "Running nmap scanning $scanrange and outputting to file $filename.txt"
 nmap -A $scanrange >> ~/nmapResult/$filename.txt &
 
