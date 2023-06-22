d563cc206aa2200fcff026628aa351b65794c004
Valo King <valo.l.king@gmail.com>
2023-06-22 09:42:04 -0400

    finished shift thing
    added list comprehension, which is cooler than the whole for-loop append
    business i was doing. also actually made it work. to decode, just input
    the shift number as a negative, which should make it reverse it's
    changes. next thing to add is insurance against a problem I think
    happens if you try to shift by something greater than 26.

--- shiftThing.py
+++ shiftThing.py
@@ -1,17 +1,22 @@
 #a list of all letters in the english language
-letters = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]
+letterList = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]
 
 #this function takes a message that must be encrypted, lowerizes it, and returns it to be assigned to a variable.
 def messageIn():
     message = input("What would you like to shift? ")
     return message.lower()
-
-messageHair = []
-for letter in messageIn():
-    messageHair.append(letter)
-
+#list comprehension fuckery that does the for loop stuff in a single line, cause that's cooler
+messageHair = [letter for letter in messageIn()]
+#taking the number to shift by
 shift = int(input("How many would you like to shift by? "))
 
+#this for-loop evaluates messageHair to see if the letter at i occurs in 
+#letterList, then gets the index of that occurance,
+#replaces the letter at i with the letter at that index+the shift number.
 for i in range(len(messageHair)):
-    messageHair[i] = letters[i+shift] 
+    if messageHair[i] in letterList:
+        index = letterList.index(messageHair[i])
+        messageHair[i] = letterList[index+shift]
+
+
 print("".join(messageHair))
