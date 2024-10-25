091154492aa8c3f5f7878a93d75a3ec0764dd113
Valo King <valo.l.king@gmail.com>
2024-10-25 16:37:16 -0400

    balls and things

--- vid2mp4.sh
+++ vid2mp4.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+read -p "Format? " FMT
 
 INFILE=$1
 OUTFILE="${1%%.*}.mp4"
-ffmpeg -i $INFILE -c:v libx265 -vtag hvc1 -crf 25 -c:a copy $OUTFILE
+ffmpeg -i $INFILE -c:v libx265 -vtag hvc1 -b:v 0 -crf 25 -c:a copy $OUTFILE
